9b6ddc9d4868506c392388ce1731de5ac13bbea4
gosa_dadi <gosa.d.dadi@gmail.com>
2023-01-14 09:32:07 -0800

    updated

--- middleWare/authMiddleware.js
+++ middleWare/authMiddleware.js
@@ -13,7 +13,6 @@ const protect = asyncHandler (async (req, res, next)=>{
         const verified =jwt.verify(token, process.env.JWT_SECRET);
         // ======get user Id from token======
         const user = await User.findById(verified.id).select("-password");
-
         if(!user){
             res.status(401)
             throw new Error("User is not found")
